e282d615db680d905a8a7ca0be7802c06d09aed5
PraveenRaja-2402 <praveen.r2402@gmail.com>
2024-02-24 19:06:18 +0530

    second commit

--- auth/(routes)/(signup)/[[...signup]]/page.tsx
+++ auth/(routes)/(signup)/[[...signup]]/page.tsx
@@ -1,24 +1,24 @@
 "use client"
 import React from 'react'
- import * as z from 'Zod'
- import { Button } from '@/components/ui/button'
- import {zodResolver} form "@hookform/resolvers/zod"
- import {useForm} from 'react-hook-form'
- import {
- Form,
- FormControl,
- FormField,
- FormItem,
- FormLabel,
- Formessage,
+import * as z from 'Zod'
+import { Button } from '@/components/ui/button'
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    Formessage,
 } from "@components/ui/form"
-import { Input } from '@/components/ui/form'
-import { FaFacebook,FaGithub,FaGoogle } from 'react-icons/fa';
+import { Input } from '@/components/ui/input'
+import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa';
 import Link from "next/link"
 const Page = () => {
-  return (
-    <div>Page</div>
-  )
+    return (
+        <div>Page</div>
+    )
 }
 
 export default Page
